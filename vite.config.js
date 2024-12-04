1fd335c3d1cebd82e5ab994321f521d844611406
hamdi zaghouan <141347852+hamdiz0@users.noreply.github.com>
2024-12-04 22:57:29 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-export default {
   build: {
     outDir: 'build', // This ensures the build is output to the 'build' directory
   },
-}
+})
